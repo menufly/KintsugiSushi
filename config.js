1d963398065a64c00d969956a4fff1bb43b028cb
pedro8897 <pedrovolei2008@gmail.com>
2026-07-11 16:03:13 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.KINTSUGI_SUPABASE = {
-  url: "https://ihjdsackjfaynbhsmqhz.supabase.co/rest/v1/",
+  url: "https://ihjdsackjfaynbhsmqhz.supabase.co",
   anonKey: "sb_publishable_qzi2on3HEywZRv2jcIQ7UQ_hkrYdX8c",
   table: "pedidos"
 };
